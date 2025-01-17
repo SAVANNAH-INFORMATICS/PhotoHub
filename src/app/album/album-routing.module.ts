0a1b8f5b967bfabafddb2519726105ebf7f7dcad
FaithAdhiambo456 <echessafaith@gmail.com>
2025-01-17 15:32:41 +0300

    Installed angular social login module and added other key components

--- src/app/album/album-routing.module.ts
+++ src/app/album/album-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import { AlbumListComponent } from './album-list/album-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: "album-detail", component: AlbumDetailComponent},
+{path: "album-list", component: AlbumListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
