5d04cdad605ebfeeb58ffa17a83a6a6bf2323379
FaithAdhiambo456 <echessafaith@gmail.com>
2025-01-23 18:05:58 +0300

    Added Auth Guard

--- src/app/Services/auth.guard.ts
+++ src/app/Services/auth.guard.ts
@@ -1,5 +1,16 @@
 import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const router = inject(Router);
+
+  
+  const authPassword = localStorage.getItem('authPassword');
+  if (authPassword) {
+    return true; 
+  } else {
+    router.navigate(['/authentication/login'], { queryParams: { returnUrl: state.url } });
+    return false; 
+  }
 };
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LandingPageComponent } from './dashboard/landing-page/landing-page.component';
 import { LoginComponent } from './authentication/login/login.component';
+import { authGuard } from './Services/auth.guard';
 
 const routes: Routes = [
   {
@@ -13,16 +14,20 @@ const routes: Routes = [
    // component: LoginComponent,
     loadChildren: () =>
       import("./authentication/authentication.module").then((m) => m.AuthenticationModule),
+    canActivate: [authGuard],
+  
   },
   {
     path: "users",
     loadChildren: () =>
-      import("./user/user.module").then((m) => m.UserModule)
+      import("./user/user.module").then((m) => m.UserModule),
+    canActivate: [authGuard],
   },
   {
     path: "albums",
     loadChildren: () =>
-      import("./album/album.module").then((m) => m.AlbumModule)
+      import("./album/album.module").then((m) => m.AlbumModule),
+    canActivate: [authGuard],
   },
   {
     path: "dashboard",
@@ -37,7 +42,8 @@ const routes: Routes = [
   {
     path: "photo",
     loadChildren: () =>
-      import("./photo/photo.module").then((m) => m.PhotoModule)
+      import("./photo/photo.module").then((m) => m.PhotoModule),
+    canActivate: [authGuard],
   },
   {
     path: "shared",
