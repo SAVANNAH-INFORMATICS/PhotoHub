26433fe54b7522422f93a776d8500173d3bd4af4
FaithAdhiambo456 <echessafaith@gmail.com>
2025-01-24 13:34:45 +0300

    debugged CSS errors, added photos in about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,34 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { LandingPageComponent } from './dashboard/landing-page/landing-page.component';
-import { LoginComponent } from './authentication/login/login.component';
-import { authGuard } from './Services/auth.guard';
 
 const routes: Routes = [
   {
     path: '', redirectTo: '/dashboard/main', pathMatch: 'full'
   },
-    {
+  {
     path: "authentication",
-   // component: LoginComponent,
+    // component: LoginComponent,
     loadChildren: () =>
       import("./authentication/authentication.module").then((m) => m.AuthenticationModule),
-    canActivate: [authGuard],
-  
   },
   {
     path: "users",
     loadChildren: () =>
       import("./user/user.module").then((m) => m.UserModule),
-    canActivate: [authGuard],
-  },
+     },
   {
     path: "albums",
     loadChildren: () =>
       import("./album/album.module").then((m) => m.AlbumModule),
-    canActivate: [authGuard],
-  },
+      },
   {
     path: "dashboard",
     loadChildren: () =>
@@ -43,14 +36,13 @@ const routes: Routes = [
     path: "photo",
     loadChildren: () =>
       import("./photo/photo.module").then((m) => m.PhotoModule),
-    canActivate: [authGuard],
-  },
+     },
   {
     path: "shared",
     loadChildren: () =>
       import("./shared/shared.module").then((m) => m.SharedModule)
   },
-  {path: "**", component: NotFoundComponent}
+  { path: "**", component: NotFoundComponent }
 ];
 
 @NgModule({
